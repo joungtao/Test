5510f894f1cfb6ff159c7fcaf81ec583a0981d1e
joungtao <joungtao@gmail.com>
2019-04-18 11:10:06 +0800

    demo 4

--- src/demo.py
+++ src/demo.py
@@ -1,15 +1,15 @@
-def calc(f, g, a, n, m):
+def calc(f, g, a):
+	i = 0
 	result = 0
-	for i in xrange(m):
-		if a >= i and a - i < n:
+	while i < len(g):
+		if a >= i and a - i < len(f):
 			result += f[a - i] * g[i]
+		i += 1
 	return result
 
-def convolution(f, g ):
-	n, m = len(f), len(g)
-	return [calc(f, g, i, n, m) for i in xrange(n)]
+def convolution(f, g):
+	return [calc(f, g, i) for i in range(len(f))]
 
-if __name__ == "__main__":
 	for _ in xrange(10000):
 		for _ in convolution([1, 2, 3, 4, 5], [1, 1, 1]):
 			pass
