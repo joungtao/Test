32eadffc607aca12226cf3ed519e98e0dbcfa954
joungtao <joungtao@gmail.com>
2019-04-18 11:05:27 +0800

    demo 1

---
+++ src/demo.py
@@ -0,0 +1,20 @@
+def calc(f, g, a):
+	i = 0
+	result = []
+	while i < len(g):
+		result.append((f[a - i] if a >= i and a - i < len(f) else 0) * g[i])
+		i += 1
+	return sum(result)
+
+def convolution(f, g=[1, 1, 1]):
+	i = 0
+	result = []
+	while i < len(f):
+		result.append(calc(f, g, i))
+		i += 1
+	return result
+
+if __name__ == "__main__":
+	for _ in range(10000):
+		for _ in convolution([1, 2, 3, 4, 5], [1, 1, 1]):
+			pass
