f25ccf5637e71c1a0e989d75cdbec9ddca98ff9c
joungtao <joungtao@gmail.com>
2019-06-11 20:14:47 +0800

    add comment

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Test
-1111111
-222222
+A demo for testing properform
+
+properform is an open source project based on tavis-ci. It provide API that can easily help developers run their projects on travis-ci with steady environment.
+
+For running properform,
+
+Step 1: Install docker
+
+Step2: Install properform with pip (pip install properform)
+
+Step3: Create/Have a git repository which is configured for travis, such as this Test project
+
+Step4: using properform with python files, such as travis.py 
--- travis.py
+++ travis.py
@@ -7,17 +7,26 @@ from properform.travis import Travis
 
 def test(git_repo, output_dir):
 	travis = Travis()
-	travis.run(git_repo,
+	travis.run(
+		# repository to be analyzed
+		git_repo,
+		#the language that is used for your project
 		language = 'python',
+		#the revision that need to be analyzed
 		commits = (
 			'4029e8373597b1ef6c8043074d26796ea01c5c02',
 		),
+		#extra command that are needed to do after running the scripts in the .travis.yml
 		extra_commands = (
 			'cp demo.prof /home/travis/output/demo.prof',
 		),
+		#bind local folder to docker
 		extra_volumes = {
 			output_dir: {'bind': '/home/travis/output', 'mode': 'rw'},
-		})
+		}
+		#more parameters:
+		#log_dir = '', mem_limit = '1G', cpu_count = 1, repeat_count = 1, yaml_files = (), extra_commands = (), extra_volumes = {}, environment = {}
+		)
 
 if __name__ == '__main__':
 	import sys
