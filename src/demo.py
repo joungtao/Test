b4f9669882b62e2b138c9653fa3e481a623de97b
joungtao <joungtao@gmail.com>
2020-07-03 10:26:23 +0800

    demo 2

--- src/demo.py
+++ src/demo.py
@@ -6,23 +6,20 @@ memory_leak.start()
 
 def calc(f, g, a):
 	i = 0
-	result = []
+	result = 0
 	while i < len(g):
-		result.append((f[a - i] if a >= i and a - i < len(f) else 0) * g[i])
+		if a >= i and a - i < len(f):
+			result += f[a - i] * g[i]
 		i += 1
-	return sum(result)
-
-def convolution(f, g=[1, 1, 1]):
-	i = 0
-	result = []
-	while i < len(f):
-		result.append(calc(f, g, i))
-		i += 1
+	json.dumps({'result': result}, indent = 4)
 	json.dumps({'result': result}, indent = 4)
 	return result
 
+def convolution(f, g):
+	return [calc(f, g, i) for i in range(len(f))]
+
 if __name__ == "__main__":
-	for _ in range(10000):
+	for _ in xrange(10000):
 		for _ in convolution([1, 2, 3, 4, 5], [1, 1, 1]):
 			pass
 
