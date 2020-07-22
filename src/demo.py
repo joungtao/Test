782660d5fb37d9668f927cbfcc8878fc86e6fe0a
joungtao <joungtao@gmail.com>
2020-07-22 15:20:07 +0800

    demo 1

--- src/demo.py
+++ src/demo.py
@@ -6,20 +6,23 @@ memory_leak.start()
 
 def calc(f, g, a):
 	i = 0
-	result = 0
+	result = []
 	while i < len(g):
-		if a >= i and a - i < len(f):
-			result += f[a - i] * g[i]
+		result.append((f[a - i] if a >= i and a - i < len(f) else 0) * g[i])
 		i += 1
-	json.dumps({'result': result}, indent = 4)
+	return sum(result)
+
+def convolution(f, g=[1, 1, 1]):
+	i = 0
+	result = []
+	while i < len(f):
+		result.append(calc(f, g, i))
+		i += 1
 	json.dumps({'result': result}, indent = 4)
 	return result
 
-def convolution(f, g):
-	return [calc(f, g, i) for i in range(len(f))]
-
 if __name__ == "__main__":
-	for _ in xrange(10000):
+	for _ in range(10000):
 		for _ in convolution([1, 2, 3, 4, 5], [1, 1, 1]):
 			pass
 
--- test.bat
+++ test.bat
@@ -2,8 +2,8 @@
 
 SET PYTHONPATH=%~dp0\..\webserver\properform\python
 
-CALL python -B src\demo.py
-CALL python -B %~dp0\commit.py localhost/app/properform/profile 0b37c65b380753a0bad936678ba9e682 demo.profile, demo.memleak
+CALL python3 -B src\demo.py
+CALL python3 -B %~dp0\commit.py localhost/app/properform/profile 0b37c65b380753a0bad936678ba9e682 demo.profile, demo.memleak
 DEL /F /Q demo.profile
 DEL /F /Q demo.memleak
 
