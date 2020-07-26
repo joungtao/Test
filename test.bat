17fbdd906617c125e6618a42334a5ff2e0b68c84
joungtao <joungtao@gmail.com>
2020-07-26 19:17:24 +0800

    ...

--- test.bat
+++ test.bat
@@ -3,7 +3,7 @@
 SET PYTHONPATH=%~dp0\..\webserver\properform\python
 
 CALL python3 -B src\demo.py
-CALL python3 -B %~dp0\commit.py localhost/app/properform/profile 0b37c65b380753a0bad936678ba9e682 demo.profile, demo.memleak
+CALL python3 -B %~dp0\commit.py localhost/app/properform/profile 0b37c65b380753a0bad936678ba9e682 demo.profile demo.memleak
 DEL /F /Q demo.profile
 DEL /F /Q demo.memleak
 
