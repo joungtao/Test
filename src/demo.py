389b42160aaaf64acf84bf9d4b129eb951ecd403
joungtao <joungtao@gmail.com>
2020-07-27 15:31:41 +0800

    demo 1

--- src/demo.py
+++ src/demo.py
@@ -6,22 +6,35 @@ memory_leak.start()
 
 def calc(f, g, a):
 	i = 0
-	result = 0
+	result = []
 	while i < len(g):
-		if a >= i and a - i < len(f):
-			result += f[a - i] * g[i]
+		result.append((f[a - i] if a >= i and a - i < len(f) else 0) * g[i])
 		i += 1
-	json.dumps({'result': result}, indent = 4)
+	return sum(result)
+
+def convolution(f, g=[1, 1, 1]):
+	i = 0
+	result = []
+	while i < len(f):
+		result.append(calc(f, g, i))
+		i += 1
 	json.dumps({'result': result}, indent = 4)
 	return result
 
-def convolution(f, g):
-	return [calc(f, g, i) for i in range(len(f))]
+class test_bound_method_A(object):
+	def foo(self):
+		pass
+
+def test_bound_method():
+	a = test_bound_method_A()
+	a.bar = a.foo
 
 if __name__ == "__main__":
 	for _ in range(10000):
 		for _ in convolution([1, 2, 3, 4, 5], [1, 1, 1]):
 			pass
+	for _ in range(5):
+		test_bound_method()
 
 profile.collect('demo.profile')
 memory_leak.collect('demo.memleak')
