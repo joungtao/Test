edbc4ffe8cb657713f171f9fb2215e8e864d4329
joungtao <joungtao@gmail.com>
2020-07-27 16:19:15 +0800

    demo 3

--- src/demo.py
+++ src/demo.py
@@ -4,19 +4,17 @@ from properform import profile, memory_leak
 profile.start()
 memory_leak.start()
 
-def calc(f, g, a):
-	i = 0
+def calc(f, g, a, n, m):
 	result = 0
-	while i < len(g):
-		if a >= i and a - i < len(f):
+	for i in range(m):
+		if a >= i and a - i < n:
 			result += f[a - i] * g[i]
-		i += 1
-	json.dumps({'result': result}, indent = 4)
-	json.dumps({'result': result}, indent = 4)
 	return result
 
-def convolution(f, g):
-	return [calc(f, g, i) for i in range(len(f))]
+def convolution(f, g ):
+	n, m = len(f), len(g)
+	json.dumps({'result': n}, indent = 4)
+	return [calc(f, g, i, n, m) for i in range(n)]
 
 class test_bound_method_A(object):
 	def foo(self):
@@ -31,6 +29,12 @@ def test_mro():
 	class B(A):pass
 	class C(B):pass
 
+def test_recursive_cellvars():
+	def recursive(n):
+		if n < 10:
+			recursive(n + 1)
+	recursive(0)
+
 if __name__ == "__main__":
 	for _ in range(10000):
 		for _ in convolution([1, 2, 3, 4, 5], [1, 1, 1]):
@@ -39,6 +43,8 @@ if __name__ == "__main__":
 		test_bound_method()
 	for _ in range(5):
 		test_mro()
+	for _ in range(5):
+		test_recursive_cellvars()
 
 profile.collect('demo.profile')
 memory_leak.collect('demo.memleak')
